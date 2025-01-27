69a4fbf709c68dc00b0ddab1963f270186ca6ebc
Renzios <61353378+Renzios@users.noreply.github.com.>
2025-01-27 22:08:51 +0800

    feat: add entry point

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+function Nav() {
+    return (
+        <nav>
+            <h1>ALBOOM</h1>
+            <input type="text" />
+            <button>Register</button>
+            <button>Sign In</button>
+        </nav>
+    )
+}
+
+export default Nav
