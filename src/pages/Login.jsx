df90977c8e6aa194fe140d7cb61bb3c49b1ef2fd
Paolo Cruzado <paolocruzado10@gmail.com>
2025-01-28 22:58:07 +0800

    Created log in page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,39 @@
-import Header from "../components/Header"
+import './Login.css';
+
 import Main from "../components/Main"
+
+import gradient from '../assets/gradient.png';
 
 function Login() {
     return (
         <>
-            <Header isAuth={false} />
             <Main>
-                Place Login Page Components Here!
+                <div className="login-page">
+                    <div className="login-box">
+                        <div className="login-image">
+                            <img src={gradient} alt="Login Illustration" />
+                        </div>
+                        <div className="login-form">
+                            <p className='p-header'>Login</p>
+                            <p className='p-body'>New to Alboom? <u>Sign up for free</u> </p>
+                            <form>
+                                <div className="input-group">
+                                    <label htmlFor="username">Username</label>
+                                    <input type="text" id="username" placeholder="Enter your username" />
+                                </div>
+                                <div className="input-group">
+                                    <label htmlFor="password">Password</label>
+                                    <input type="password" id="password" placeholder="Enter your password" />
+                                </div>
+                                <p className='p-body'><u>Forgot your password?</u> </p>
+                                <button type="submit" className="login-button">Log In</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </Main>
         </>
-    )
+    );
 }
 
 export default Login
