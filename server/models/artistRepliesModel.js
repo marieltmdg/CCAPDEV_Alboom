9554db375a7ab51741878945fab3c774918f8b1e
marieltmdg <marielmollytamondong@gmail.com>
2025-03-11 09:55:42 +0800

    Delete artistRepliesModel.js

--- server/models/artistRepliesModel.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const artistRepliesSchema = new mongoose.Schema({
-    review_id: { type: mongoose.Schema.Types.ObjectId, ref: "Review", required: true },
-    artist_id: { type: mongoose.Schema.Types.ObjectId, ref: "Artist", required: true },
-    reply_text: { type: String, required: true }
-},);
