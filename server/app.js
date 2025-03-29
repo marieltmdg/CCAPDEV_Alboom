a0f57ce9a8aff2ff5de69bc7278078e73bbbf1e7
marieltmdg <marielmollytamondong@gmail.com>
2025-03-29 23:20:14 +0800

    Update app.js

--- server/app.js
+++ server/app.js
@@ -37,7 +37,7 @@ app.use((err, req, res, next) => {
     res.status(500).json({ message: err.message });
 });
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, async () => {
     console.log(`Server running on port ${PORT}`);
 });
