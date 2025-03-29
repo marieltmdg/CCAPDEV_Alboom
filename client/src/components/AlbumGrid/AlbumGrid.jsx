6e27b8d6ff331d521bc952a9567b40e50f7f4b58
marieltmdg <marielmollytamondong@gmail.com>
2025-03-29 17:01:04 +0800

    fix: remove localhost

--- client/src/components/AlbumGrid/AlbumGrid.jsx
+++ client/src/components/AlbumGrid/AlbumGrid.jsx
@@ -1,16 +1,19 @@
-import styles from './AlbumGrid.module.css'
+import styles from './AlbumGrid.module.css';
 
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
-function AlbumGrid({albums}) {
- 
+function AlbumGrid({ albums }) {
+    const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+
     return (
         <div className={styles.container}>
             {albums.map(album => (
-                <Link to={"/album/" + album._id} key={album._id} className={styles.item}><img src={"http://localhost:3000/" + album.cover} alt="" className={styles.cover} /></Link>
+                <Link to={"/album/" + album._id} key={album._id} className={styles.item}>
+                    <img src={`${apiBaseUrl}${album.cover}`} alt="" className={styles.cover} />
+                </Link>
             ))}
         </div>
-    )
+    );
 }
 
-export default AlbumGrid
+export default AlbumGrid;
