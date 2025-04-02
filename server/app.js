6d076a8ec839b7981ff1e9f3b0cdc01053610af6
marieltmdg <marielmollytamondong@gmail.com>
2025-04-02 22:37:40 +0800

    fix: reload

--- server/app.js
+++ server/app.js
@@ -32,6 +32,7 @@ app.use(cors({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
 app.use(express.static("public")); 
+app.use(express.static("build"));
 app.use(fileUpload());
 
 const sessionStore = MongoStore.create({
