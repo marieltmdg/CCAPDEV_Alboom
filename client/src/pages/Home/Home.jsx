8678f42bd6783142f5f221b96b2fa9eda96f3a50
Paolo Cruzado <paolocruzado10@gmail.com>
2025-04-06 15:04:30 +0800

    fix: labels in filter menu

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -149,14 +149,15 @@ function Home() {
                         <div className={styles.close} onClick={toggleFilterMenu}>
                             <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289Z" fill="#ffffff"></path> </g></svg>
                         </div>
+                        <h1>Sort</h1>
+                        <div className={styles.button} onClick={sortByHighestRating}>Highest to Lowest Booms</div>
+                        <div className={styles.button} onClick={sortByLowestRating}>Lowest to Highest Booms</div>
                         <h1>Filter</h1>
-                        <div className={styles.button} onClick={sortByHighestRating}>Highest to Lowest Rating</div>
-                        <div className={styles.button} onClick={sortByLowestRating}>Lowest to Highest Rating</div>
                         <input
                             type="number"
                             id="filterByNumber"
                             className={styles.booms}
-                            placeholder="Number of Booms"
+                            placeholder="Input Number of Booms"
                             value={filterByNumber}
                             onChange={(e) => {
                                 const value = e.target.value;
@@ -166,7 +167,7 @@ function Home() {
                             }} 
                         />
                         <div className={styles.button} onClick={handleFilterByNumber}> Number of Booms </div>
-                        <div className={styles.button} onClick={resetFilter}> Reset Filter </div>
+                        <div className={styles.buttonAlt} onClick={resetFilter}> Reset Filter </div>
                     </div>
                 )}
 
