7ae4e9b655d0666750678b84da65601dd6b8c87f
Paolo Cruzado <paolocruzado10@gmail.com>
2025-04-07 23:04:34 +0800

    feat: add filtering by num of upvotes

--- client/src/pages/Album/Album.jsx
+++ client/src/pages/Album/Album.jsx
@@ -23,7 +23,7 @@ function Album() {
     const [isFilterMenuOpen, setIsFilterMenuOpen] = useState(false); 
     const [isFadingOut, setIsFadingOut] = useState(false);
     const [filterByNumber, setFilterByNumber] = useState(""); 
-    const { authState} = useAuth(); // Get setAuthState from context
+    const {authState} = useAuth(); 
 
     const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
 
@@ -157,6 +157,20 @@ function Album() {
         setReviews(originalReviews);  
     };
 
+    const sortByHighestUpvote = () => {
+        const sorted = [...reviews].sort((a, b) => {
+            return b.upvotes - a.upvotes;  // Sorts in descending order of upvotes
+        });
+        setReviews(sorted);
+    };
+    
+    const sortByLowestUpvote = () => {
+        const sorted = [...reviews].sort((a, b) => {
+            return a.upvotes - b.upvotes;  // Sorts in ascending order of upvotes
+        });
+        setReviews(sorted);
+    };
+
     const toggleFilterMenu = () => {
         if (isFilterMenuOpen) {
             setIsFadingOut(true);
@@ -201,8 +215,24 @@ function Album() {
                             <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289Z" fill="#ffffff"></path> </g></svg>
                         </div>
                         <h1>Sort</h1>
-                        <div className={styles.buttonFilter} onClick={sortByHighestRating}>Highest to Lowest Booms</div>
-                        <div className={styles.buttonFilter} onClick={sortByLowestRating}>Lowest to Highest Booms</div>
+                        <div className={styles.row}>
+                            <h2>Booms:</h2>
+                            <div className={styles.smallButton} onClick={sortByHighestRating}>
+                                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M12 3C12.2652 3 12.5196 3.10536 12.7071 3.29289L19.7071 10.2929C20.0976 10.6834 20.0976 11.3166 19.7071 11.7071C19.3166 12.0976 18.6834 12.0976 18.2929 11.7071L13 6.41421V20C13 20.5523 12.5523 21 12 21C11.4477 21 11 20.5523 11 20V6.41421L5.70711 11.7071C5.31658 12.0976 4.68342 12.0976 4.29289 11.7071C3.90237 11.3166 3.90237 10.6834 4.29289 10.2929L11.2929 3.29289C11.4804 3.10536 11.7348 3 12 3Z" fill="#ffffff"></path> </g></svg>
+                            </div>
+                            <div className={styles.smallButton} onClick={sortByLowestRating}>
+                                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M12 3C12.5523 3 13 3.44772 13 4V17.5858L18.2929 12.2929C18.6834 11.9024 19.3166 11.9024 19.7071 12.2929C20.0976 12.6834 20.0976 13.3166 19.7071 13.7071L12.7071 20.7071C12.3166 21.0976 11.6834 21.0976 11.2929 20.7071L4.29289 13.7071C3.90237 13.3166 3.90237 12.6834 4.29289 12.2929C4.68342 11.9024 5.31658 11.9024 5.70711 12.2929L11 17.5858V4C11 3.44772 11.4477 3 12 3Z" fill="#ffffff"></path> </g></svg>
+                            </div>
+                        </div>
+                        <div className={styles.row}>
+                            <h2>Upvotes:</h2>
+                            <div className={styles.smallButton} onClick={sortByHighestUpvote}>
+                                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M12 3C12.2652 3 12.5196 3.10536 12.7071 3.29289L19.7071 10.2929C20.0976 10.6834 20.0976 11.3166 19.7071 11.7071C19.3166 12.0976 18.6834 12.0976 18.2929 11.7071L13 6.41421V20C13 20.5523 12.5523 21 12 21C11.4477 21 11 20.5523 11 20V6.41421L5.70711 11.7071C5.31658 12.0976 4.68342 12.0976 4.29289 11.7071C3.90237 11.3166 3.90237 10.6834 4.29289 10.2929L11.2929 3.29289C11.4804 3.10536 11.7348 3 12 3Z" fill="#ffffff"></path> </g></svg>
+                            </div>
+                            <div className={styles.smallButton} onClick={sortByLowestUpvote}>
+                                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M12 3C12.5523 3 13 3.44772 13 4V17.5858L18.2929 12.2929C18.6834 11.9024 19.3166 11.9024 19.7071 12.2929C20.0976 12.6834 20.0976 13.3166 19.7071 13.7071L12.7071 20.7071C12.3166 21.0976 11.6834 21.0976 11.2929 20.7071L4.29289 13.7071C3.90237 13.3166 3.90237 12.6834 4.29289 12.2929C4.68342 11.9024 5.31658 11.9024 5.70711 12.2929L11 17.5858V4C11 3.44772 11.4477 3 12 3Z" fill="#ffffff"></path> </g></svg>
+                            </div>
+                        </div>
                         <h1>Filter</h1>
                         <input
                             type="number"
